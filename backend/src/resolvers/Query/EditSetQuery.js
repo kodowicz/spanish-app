598685c563e734408af0a2083f53558d34c61c1f
kodowicz <annakodowicz@gmail.com>
2021-03-17 10:21:35 +0100

    create update and delete EditSet

---
+++ backend/src/resolvers/Query/EditSetQuery.js
@@ -0,0 +1,29 @@
+const editSet = async (_parent, _args, context, info) => {
+  const userid = context.request.userid || context.request.headers.userid;
+  if (!userid) {
+    throw new Error('You have to be logged');
+  }
+
+  const user = await context.prisma.query.user(
+    { where: { id: userid } },
+    `{ editSet { id }}`
+  );
+  if (!user.editSet) {
+    throw new Error(`You don't have any set to update`);
+  }
+
+  const editSet = await context.prisma.query.editSet(
+    {
+      where: {
+        id: user.editSet.id
+      }
+    },
+    info
+  );
+
+  return editSet;
+};
+
+module.exports = {
+  editSet
+};
