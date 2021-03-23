1a4e2b1f7c0db780e3345a8a12c6fab0353e5511
kodowicz <annakodowicz@gmail.com>
2021-03-23 17:42:03 +0100

    update Set and related LearnSets

--- backend/src/resolvers/Mutation/SetMutation.js
+++ backend/src/resolvers/Mutation/SetMutation.js
@@ -137,42 +137,164 @@ const updateSet = async (_parent, _args, context, info) => {
     throw new Error('You have to create at least 4 terms');
   }
 
-  const deleteManyTerms = user.editSet.set.terms
-    .filter(
-      term =>
-        !formatedTerms.find(editTerm => {
-          if (editTerm.term) {
-            return editTerm.term.id == term.id;
-          }
-        })
-    )
-    .map(term => ({ id: term.id }));
-
-  const upsertManyTerms = formatedTerms
-    .filter(editTerm => {
-      if (editTerm.term) {
-        if (
-          editTerm.spanish !== editTerm.term.spanish ||
-          editTerm.english !== editTerm.term.english
-        ) {
-          return editTerm;
+  const termsToDelete = user.editSet.set.terms.filter(
+    term =>
+      !formatedTerms.find(editTerm => {
+        if (editTerm.term) {
+          return editTerm.term.id == term.id;
         }
-      } else {
-        return editTerm;
+      })
+  );
+
+  const learnSetsToUpdate = await context.prisma.query.learnSets(
+    {
+      where: {
+        set: {
+          id: user.editSet.set.id
+        }
+      }
+    },
+    `{ id }`
+  );
+
+  const learnTermsToDelete = await Promise.all(
+    termsToDelete.map(async term => {
+      const learnTerms = await context.prisma.query.learnTerms(
+        { where: { term: { id: term.id } } },
+        `{
+          id
+          learnSet {
+            id
+          }
+        }`
+      );
+      return learnTerms;
+    })
+  );
+
+  const learnTermsToUpdate = await Promise.all(
+    formatedTerms.map(async editTerm => {
+      if (editTerm.term) {
+        const termid = editTerm.term.id;
+        const learnTerms = await context.prisma.query.learnTerms(
+          { where: { term: { id: termid } } },
+          `{ id }`
+        );
+
+        return {
+          id: termid,
+          learnTerms: learnTerms.map(term => term.id)
+        };
       }
     })
-    .map(editTerm => {
-      const { id, term, ...data } = editTerm;
-      const termid = (term && term.id) || '';
+  );
 
-      return {
-        where: {
-          id: termid
-        },
-        update: data,
-        create: data
-      };
-    });
+  const learnTermsToCreate = await Promise.all(
+    formatedTerms.flatMap(async editTerm => {
+      if (!editTerm.term) {
+        const terms = await context.prisma.query.learnSets(
+          { where: { set: { id: user.editSet.set.id } } },
+          `{ id }`
+        );
+        if (terms) {
+          return terms.map(term => term.id);
+        }
+      }
+    })
+  );
+
+  const deleteManyTerms = termsToDelete.map(term => ({ id: term.id }));
+
+  const upsertManyTerms = formatedTerms.map(editTerm => {
+    const { id, term, ...data } = editTerm;
+    const termid = (term && term.id) || '';
+
+    let updateManyLearnTerms = [];
+    let createManyLearnTerms = [];
+
+    // update LearnTerm if Term exists of create a new one
+    if (termid) {
+      learnTermsToUpdate
+        .filter(term => term)
+        .forEach(term => {
+          if (term.id === termid) {
+            term.learnTerms.forEach(learnTermId => {
+              updateManyLearnTerms.push({
+                where: {
+                  id: learnTermId
+                },
+                data: {
+                  ...data,
+                  ratio: 0,
+                  mastered: false
+                }
+              });
+            });
+          }
+        });
+    } else {
+      if (learnTermsToCreate) {
+        learnTermsToCreate
+          .flatMap(arr => arr)
+          .filter((item, index, arr) => arr.indexOf(item) == index && item)
+          .forEach(learnSetId => {
+            createManyLearnTerms.push({
+              spanish: editTerm.spanish,
+              english: editTerm.english,
+              learnSet: {
+                connect: {
+                  id: learnSetId
+                }
+              }
+            });
+          });
+      }
+    }
+
+    return {
+      where: {
+        id: termid
+      },
+      update: {
+        ...data,
+        learnTerms: {
+          update: updateManyLearnTerms
+        }
+      },
+      create: {
+        ...data,
+        learnTerms: {
+          create: createManyLearnTerms
+        }
+      }
+    };
+  });
+
+  const updateManyLearnSets = learnSetsToUpdate.map(learnSet => {
+    const deleteManyLearnTerms = learnTermsToDelete
+      .flatMap(arr => arr)
+      .map(learnTerm => {
+        if (learnTerm.learnSet.id === learnSet.id) {
+          return {
+            id: learnTerm.id
+          };
+        }
+      })
+      .filter(term => term);
+
+    return {
+      where: {
+        id: learnSet.id
+      },
+      data: {
+        title: user.editSet.title,
+        amount: formatedTerms.length,
+        learnTerms: {
+          delete: deleteManyLearnTerms
+        }
+      }
+    };
+  });
 
   const set = await context.prisma.mutation.updateSet(
     {
@@ -182,6 +304,9 @@ const updateSet = async (_parent, _args, context, info) => {
         terms: {
           deleteMany: deleteManyTerms,
           upsert: upsertManyTerms
+        },
+        learnSets: {
+          update: updateManyLearnSets
         }
       },
       where: {
